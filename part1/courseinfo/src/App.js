4c8a901457cb7015da5a1519f27391478563cb32
Vladimir Mishenskiy <vmishensky6@gmail.com>
2023-04-11 11:33:26 +0400

    pt1 exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,11 +9,11 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/> 
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
-  ) 
+  )
 }
 
 const Header = (props) => {
@@ -27,10 +27,28 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part1 part1={props.part1} exercises1={props.exercises1} />
+      <Part2 part2={props.part2} exercises2={props.exercises2} />
+      <Part3 part3={props.part3} exercises3={props.exercises3} />
     </div>
+  )
+}
+
+const Part1 = (props) => {
+  return (
+    <p>{props.part1} {props.exercises1}</p>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <p>{props.part2} {props.exercises2}</p>
+  )
+}
+
+const Part3 = (props) => {
+  return (
+    <p>{props.part3} {props.exercises3}</p>
   )
 }
 
