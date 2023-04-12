190d48e74c7b61c692ebb535c671dd12195df35e
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-04-12 21:22:07 +0400

    part 1c before "event handling" part

--- part1/part1c/src/App.js
+++ part1/part1c/src/App.js
@@ -1,8 +1,43 @@
-const Hello = (props) => {
-  const name = props.name
-  const age = props.age
+import { useState } from "react";
 
+const App = (props) => {
+  // const {counter} = props
+
+  const [counter, setCounter] = useState(0)
+  
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
+
+  return (
+    <div>{counter}</div>
+  )
+
+  // PART1C - COMPONENT HELPER FUNCTIONS AND DESCTRUCTURING //
+  // const name = 'Peter'
+  // const age = 10
+
+  // return (
+  //   <div>
+  //     <h1>Greetings</h1>
+  //     <Hello name='Maya' age = {26+10}/>
+  //     <Hello name={name} age={age}/>
+  //   </div>
+  // )
+}
+
+// PART1C - DESTRUCTURING //
+/* const Hello = ({name, age}) => { // props in parameters changed to {name, age}
   const bornYear = () => new Date().getFullYear() - age
+
+  // DESCTRUCTURING // 
+  // const {name, age} = props
+
+  // const name = props.name
+  // const age = props.age
 
   // const bornYear = () => {
   //   const yearNow = new Date().getFullYear()
@@ -15,19 +50,6 @@ const Hello = (props) => {
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age = {26+10}/>
-      <Hello name={name} age={age}/>
-    </div>
-  )
-}
+} */
 
 export default App;
