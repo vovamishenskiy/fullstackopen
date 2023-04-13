2d5b299a891923109f53a3879701f4b171cb92d4
Vladimir Mishenskiy <60189954+vovamishenskiy@users.noreply.github.com>
2023-04-13 12:01:57 +0400

    part1d before 'rules of hooks'

--- part1/part1d/src/App.js
+++ part1/part1d/src/App.js
@@ -30,24 +30,32 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
+    setTotal(left + right)
   }
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      {/* <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button> */}
+      <Button handleClick={handleLeftClick} text='left'/>
+      <Button handleClick={handleRightClick} text='right'/>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      {/* <p>{allClicks.join(' ')}</p>
+      <p>total: {total}</p> */}
+      <History allClicks={allClicks}/>
     </div>
 
     // <div>
@@ -57,6 +65,21 @@ const App = () => {
     //   <button onClick={() => setRight(right+1)}>right</button>
     //   {right}
     // </div>
+  )
+}
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const History = (props) => {
+  if(props.allClicks.length === 0) {
+    return (
+      <div>the app is used by pressing the buttons</div>
+    )
+  }
+  return (
+    <div>button press history: {props.allClicks.join(' ')}</div>
   )
 }
 
