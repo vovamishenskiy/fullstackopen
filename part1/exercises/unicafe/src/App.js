4a663b72ad54241ed14a7291d495270ad7d4e313
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-04-13 18:55:14 +0400

    unicafe - exercise 1.6 done

--- part1/exercises/unicafe/src/App.js
+++ part1/exercises/unicafe/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"; 
 
-function App() {
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // DEBUGGING HANDLERS //
+  /* const handleGoodClick = () => {
+    console.log('setting new good, good before: ', good)
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    console.log('setting new neutral, neutral before: ', neutral)
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    console.log('setting new bad, bad before: ', bad)
+    setBad(bad + 1)
+  } */
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+
+      <h1>statistics</h1>
+      <p>
+        good {good} <br/>
+        neutral {neutral} <br/>
+        bad {bad}
+      </p>
     </div>
-  );
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
 }
 
 export default App;
