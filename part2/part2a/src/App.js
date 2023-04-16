839cef1fe44f8359182672197e18529701ade5ab
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-04-16 17:22:42 +0400

    part2a upd.

--- part2/part2a/src/App.js
+++ part2/part2a/src/App.js
@@ -1,10 +1,12 @@
+import Note from './components/Note';
+
 const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
+          <Note key={note.id} note={note}/>
         ))}
       </ul>
     </div>
