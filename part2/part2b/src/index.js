8c7595bb85ffb3a4e99c80f1699e1eb383e45378
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-04-22 18:11:40 +0400

    part2c started up until axios get in App component

--- part2/part2b/src/index.js
+++ part2/part2b/src/index.js
@@ -1,25 +1,38 @@
 import ReactDOM from "react-dom/client"
-
 import App from "./App"
+import React from "react"
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-]
+// const notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ]
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-)
+// axios.get('http://localhost:3001/notes').then(response => {
+//   const notes = response.data
+//   ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+// })
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+// const promise = axios.get('http://localhost:3001/notes')
+
+// promise.then(response => {
+//   console.log(response)
+// })
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <App notes={notes} />
+// )
