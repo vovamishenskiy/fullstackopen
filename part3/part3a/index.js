5c67945f6bdc910a770c7cfc84446aa72ac91319
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-05-14 21:47:19 +0400

    part3b started

--- part3/part3a/index.js
+++ part3/part3a/index.js
@@ -1,35 +1,8 @@
-// const http = require('http')
-
-// let notes = [
-//     {
-//         id: 1,
-//         content: "HTML is easy",
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: "Browser can execute only JavaScript",
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         important: true
-//     }
-// ]
-
-// const app = http.createServer((request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'application/json' })
-//     response.end(JSON.stringify(notes))
-// })
-
-// const PORT = 3001
-// app.listen(PORT)
-// console.log(`Server is running on port ${PORT}`)
-
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 let notes = [
     {
@@ -98,7 +71,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
